5a5d3ed770741b9c9eece02f6b8e2428d363d340
Homebrew Tap <ci@julienvincent.io>
2026-03-07 18:14:44 +0000

    Update pruner to v1.0.0-alpha.10

--- Formula/pruner.rb
+++ Formula/pruner.rb
@@ -2,27 +2,27 @@ class Pruner < Formula
   desc "Code formatter orchestrator built with Treesitter"
   homepage "https://github.com/pruner-formatter/pruner"
   license "MIT"
-  version "1.0.0-alpha.9"
+  version "1.0.0-alpha.10"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/pruner-formatter/pruner/releases/download/v1.0.0-alpha.9/pruner-macos-amd64"
-      sha256 "776d9debe47312f652a4fdee19b050f8b5fcdbfbcbfeb120fee40cfef9d709ed"
+      url "https://github.com/pruner-formatter/pruner/releases/download/v1.0.0-alpha.10/pruner-macos-amd64"
+      sha256 "3cdbcc9955b742f6778466ace748d694ca63d2e994da5ae147aed99dceaf6b67"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/pruner-formatter/pruner/releases/download/v1.0.0-alpha.9/pruner-macos-arm64"
-      sha256 "5ed60c32da8dce3bcd7c417369f07e1ddd9f641ef30bb93b31fd82d0b7cae4f7"
+      url "https://github.com/pruner-formatter/pruner/releases/download/v1.0.0-alpha.10/pruner-macos-arm64"
+      sha256 "77649ba9e753dd694fca95696f5a348169645534ff56578293efdfcd6e837674"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/pruner-formatter/pruner/releases/download/v1.0.0-alpha.9/pruner-linux-amd64"
-      sha256 "93968d8a9b507e3c85ee1ae6b7dcf9d4eb8ebde2e47340e8f0a32862f70864d0"
+      url "https://github.com/pruner-formatter/pruner/releases/download/v1.0.0-alpha.10/pruner-linux-amd64"
+      sha256 "4e6384b67ccc02184ca372ee358da4b53178ef41b1484024703640fc4cd01437"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/pruner-formatter/pruner/releases/download/v1.0.0-alpha.9/pruner-linux-arm64"
-      sha256 "ea224f83008d585a6e7740a957a2e0f4f55faae7c98e0897c7ae41d18104d1fc"
+      url "https://github.com/pruner-formatter/pruner/releases/download/v1.0.0-alpha.10/pruner-linux-arm64"
+      sha256 "5929fcbcc3fbbdfc28002f1995ac20f59204a16ed71d5417b9b17eefaf55180f"
     end
   end
 
